0e1613e83aa944a48a26480f1f438a6e51d8cf29
Maulana Muhammad <maulanamuhammad2000@gmail.com>
2026-09-01 17:48:30 +0700

    feat: implement base layout with global CSS and site favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
-
-	let { children } = $props();
+  import './layout.css';
+  let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head><link rel="icon" href="/logo.png" /></svelte:head>
 {@render children()}
